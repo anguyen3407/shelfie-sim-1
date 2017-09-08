a78e75ede5fe72651ceefe00fabedee402f1c948
Trevor <TrevorBrown25@gmail.com>
2017-09-08 10:34:50 -0700

    created the server

---
+++ shelfie/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+const ctrl = require('./controller');
+
+require('dotenv').config();
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db);
+}).catch(err => console.log(err));
+
+
+
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`listening on port ${port}`));
